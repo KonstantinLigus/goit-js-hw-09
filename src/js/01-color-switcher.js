4aea303b9bef340c3e1e2a2ad5069b9cd36e4b33
Konstantin Ligus <kostya1989com@gmail.com>
2022-05-17 14:33:36 +0300

    task 1 have done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+refs.startBtn.addEventListener('click', handleStartBtnClick);
+refs.stopBtn.addEventListener('click', handleStoptBtnClick);
 
+let intervalId = null;
+
+function handleStartBtnClick(event) {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  event.target.setAttribute('disabled', 'true');
+}
+
+function handleStoptBtnClick() {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
